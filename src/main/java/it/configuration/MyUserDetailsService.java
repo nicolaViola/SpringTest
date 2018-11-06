26a9682bf3039e6a99607485197e0e4d75d1998e
Viola_N <nicola.viola@regione.emilia-romagna.it>
2018-11-06 15:47:03 +0100

    implementato un UserDetailsService per uno storeging custom da agganciare alla configurazione

--- src/main/java/it/configuration/MyUserDetailsService.java
+++ src/main/java/it/configuration/MyUserDetailsService.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class MyUserDetailsService implements UserDetailsService{
+public class MyUserDetailsService implements UserDetailsService{ 
 
 	@Override
 	public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
