343ab9843af2046d8feab3a0eaeeec17622be455
nicolaViola <nicola.viola78@gmail.com>
2018-12-03 08:35:53 +0100

    test jdbc + aggiunta supporto transazionale per jpa

--- src/main/java/it/configuration/TestConfiguration.java
+++ src/main/java/it/configuration/TestConfiguration.java
@@ -2,10 +2,19 @@ package it.configuration;
 
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import it.servicies.test.Figlio;
 import it.servicies.test.Figlio1;
 import it.servicies.test.Padre;
+
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
@@ -16,16 +25,48 @@ import org.springframework.context.annotation.*;
 //@ComponentScan(basePackages={"it"})
 
 @PropertySource("classpath:property/nicola.properties")
-@EnableAspectJAutoProxy
+//@EnableAspectJAutoProxy
+
+@EnableTransactionManagement
 public class TestConfiguration {
 //	@Autowired
 //	private Figlio figlio;
+	
+	public PlatformTransactionManager transactionManager(){
+		
+		return new JpaTransactionManager();
+	}
 	
 	@Bean
 	public PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer(){
 		return new PropertySourcesPlaceholderConfigurer();
 	}
 	
+	@Bean("xxx")
+	public DataSource xxx() {
+		
+//		PasswordEncoder bCryptPasswordEncoder = passwordEncoder();
+//		//String p = bCryptPasswordEncoder.encode("admin");
+//		System.out.println(bCryptPasswordEncoder.matches("admin", "$2a$08$YN2y9ehNoknkvyPn5iVNEeb8qnCO5VDKs7FkmoM8hQjt2IZh/ACza"));
+//		//System.out.println(p);
+		
+		
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+		EmbeddedDatabase db = builder
+			.setType(EmbeddedDatabaseType.H2) //.H2 or .DERBY
+			.addScripts("classpath:db/schema.sql", 
+						"classpath:db/test-data.sql")
+			.build();
+		return db;
+	}
+	
+	
+	@Bean
+	public JdbcTemplate myJdbcTemplate(){
+		
+		return new JdbcTemplate(xxx());
+		
+	}
 
 //	@Bean//(initMethod="initialize3")
 //	public Figlio figlio(){
--- src/main/test/resttest/MyJunitTest5.java
+++ src/main/test/resttest/MyJunitTest5.java
@@ -2,15 +2,27 @@ package resttest;
 
 import static org.junit.Assert.*;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Optional;
+
+import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.env.Environment;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -19,6 +31,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import it.configuration.TestConfiguration;
+import it.model.User;
 import it.servicies.ITrackerService;
 import it.servicies.impl.TrackerService;
 import it.servicies.test.Altro;
@@ -57,7 +70,7 @@ public class MyJunitTest5 {
 //	@Autowired
 //	private Padre[] padri;
 
-	@Test
+	//@Test
 	public void test() {
 		trackerService.forTest();
 		Figlio figlio = new Figlio();
@@ -67,7 +80,7 @@ public class MyJunitTest5 {
 		System.out.println(figlio);
 	}
 	
-	@Test
+	//@Test
 	public void testPadre() {
 		//Figlio figlio = padre.getFiglio();
 	//	System.out.println(padre);
@@ -78,7 +91,7 @@ public class MyJunitTest5 {
 		
 		
 	}
-	@Test
+//	@Test
 	public void testApplicationContext() {
 		ApplicationContext context = new AnnotationConfigApplicationContext(TestConfiguration.class);
 		Environment env = context.getEnvironment();
@@ -101,7 +114,7 @@ public class MyJunitTest5 {
 	@Autowired
 	private Tester tester;
 	
-	@Test
+	//@Test
 	public void testAop() throws Exception {
 //		int y = padre.getRandomNumber();
 //		System.out.println(y);
@@ -115,5 +128,81 @@ public class MyJunitTest5 {
 		
 		System.out.println("fine");
 	}
+	
+	@Autowired
+	JdbcTemplate myJdbcTemplate;
+	
+	@Test
+	public void testJdbcRowMapper() throws Exception {
+		
+		
+		Collection<User> users = (Collection<User>) myJdbcTemplate.query("select username, password, enabled from users ", new RowMapper<User>() {
+			
+			
+			@Override
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new User(rs.getString(1), rs.getString(2), rs.getBoolean(3));
+			}
+		});
+		
+		for(User user : users) {
+			System.out.println(user.getUsername());
+			
+		}
+	}
+	
+	@Test
+	public void testJdbcRowcalBackHandler() throws Exception {
+		
+		myJdbcTemplate.query("select username, password, enabled from users", new RowCallbackHandler() {
+			
+			@Override
+			public void processRow(ResultSet rs) throws SQLException {
+				//while(rs.next())
+					System.out.println(rs.getString(1));
+				
+			}
+		});
+		
+		
+	}
+	
+	@Autowired
+	private DataSource xxx;
+	
+	@Test
+	public  void testJdbcResultSetExtractor() throws Exception {
+		JdbcTemplate t = new JdbcTemplate(xxx);
+		
+		t.queryForList("select username from users", String.class);
+		
+		t.queryForList("select username, password, enabled from users");
+		
+		//myJdbcTemplate.queryForList("select username, password, enabled from users");
+		
+		
+		
+		Collection<User> users = myJdbcTemplate.query("select username, password, enabled from users", new ResultSetExtractor<Collection<User>>() {
+
+			@Override
+			public Collection<User> extractData(ResultSet rs) throws SQLException, DataAccessException {
+				// TODO Auto-generated method stub
+				Collection<User> users = new ArrayList<>();
+				while(rs.next()){
+					users.add(new User(rs.getString(1), rs.getString(2), rs.getBoolean(3)));
+					
+				}
+				return users;
+			}
+		});
+		
+		for(User user : users) {
+			System.out.println(user.getUsername());
+			
+		}
+	}
 
 }
+
+
+
